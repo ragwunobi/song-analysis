1ff39b9c623c7f43f62088cd09b7c7374cd65006
ragwunobi <166466594+ragwunobi@users.noreply.github.com>
2024-06-02 23:29:50 -0400

    feat: Added get_songs(name) function to search Genius API for songs by artist name
    - Added get_songs(name) function to search API by artist name
    - Takes in an artist name (or search term) as input and returns list of song names and paths.
    - Limited to top 500 songs.
    - Should include songs they appeared in as features.
    - Planning to use paths of songs and  beautifulsoup library to create a new function that gets the lyrics of each song.

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,50 @@
 from flask import Flask, jsonify
+import requests
+import json 
 
+with open('config.json') as file: 
+    config=json.load(file) 
+
+#API Keys 
+client_id = config['client_id'] 
+client_secret = config['client_secret'] 
+client_access = config['client_access'] 
+bearer = config['bearer_token']
+headers = {
+        "Authorization": f"Bearer {bearer}"
+   }
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello from Flask!'
 
-@app.route('/api/data')
-def get_data():
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
+#Get song titles and paths 
+@app.route('/songs/<name>')
+def get_songs(name):
+   url = "http://api.genius.com/search?q="+name 
+   params = {
+       "per_page":50, 
+       "page": "1" 
+    }
+   song_data = [] 
+   while True: 
+      response = requests.get(url, headers=headers, params=params)
+      #limit to 500 songs
+      if not response or len(song_data) >= 500: 
+         break 
+      response = response.json()["response"]
+      for song in response["hits"]:
+         result = song["result"]
+         title = ""
+         path = ""
+         if result["full_title"]: 
+           title = result["full_title"] 
+         if result["path"]: 
+            path = result["path"] 
+         song_data.append((title,path)) 
+   params["page"] = str(int(params["page"]) + 1) 
+   return song_data
 
 if __name__ == '__main__':
     app.run(debug=True)
