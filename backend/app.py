a85333145e78ced3bd9a648836556c5e0033d311
ragwunobi <166466594+ragwunobi@users.noreply.github.com>
2024-06-04 06:59:17 -0400

    feat: Extract lyrics from a song's Genius page, using Beautiful Soup and regex.
    feat:
    - Implemented a function, using Beautiful Soup, to parse a song's Genius page for lyrics.
    - Used regex to parse lyrics from page's content, which typically starts with 'Lyrics[' and ends with a string that matches r"[0-9][\w\s]+Embed".
    
    todo:
    - Add additional formatting to song_lyrics(), like inserting newlines.
    - Implement fixes where lyrics are not found.
    - Add a function that uses NLP to eliminate explicit content from lyrics.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from bs4 import BeautifulSoup
 import requests
 import json
+import re
 
 with open("config.json") as file:
     config = json.load(file)
@@ -14,34 +16,61 @@ headers = {"Authorization": f"Bearer {bearer}"}
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello from Flask!"
-
-
 # Get song titles and paths
 @app.route("/songs/<name>")
 def get_songs(name):
     url = "http://api.genius.com/search?q=" + name
     params = {"per_page": 50, "page": "1"}
     song_data = []
-    while True:
-        response = requests.get(url, headers=headers, params=params)
-        # limited to 500 songs
-        if not response or len(song_data) >= 500:
-            break
-        response = response.json()["response"]
-        for song in response["hits"]:
-            result = song["result"]
-            title = ""
-            path = ""
-            if result["full_title"]:
-                title = result["full_title"]
-            if result["path"]:
-                path = result["path"]
-            song_data.append((title, path))
-    params["page"] = str(int(params["page"]) + 1)
+    try:
+        requests_count = 0
+        while requests_count <= 10:
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            response = response.json()["response"]
+            for song in response["hits"]:
+                result = song["result"]
+                title = ""
+                path = ""
+                if result["full_title"]:
+                    title = result["full_title"]
+                if result["path"]:
+                    path = result["path"]
+                lyrics = song_lyrics(path)
+                if lyrics:
+                    song_data.append([title, path, lyrics])
+                else:
+                    song_data.append([title, path, "Lyrics not found."])
+                requests_count += 1
+            params["page"] = str(int(params["page"]) + 1)
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+    except:
+        raise Exception(
+            name + " discography could not be found. Please try another request."
+        )
     return song_data
+
+
+# Get song lyrics from path
+def song_lyrics(path):
+    url = "https://genius.com" + path
+    try:
+        response = requests.get(url, headers=headers)
+    except:
+        raise Exception("Lyrics not found")
+    soup = BeautifulSoup(response.text, "html.parser")
+    content = soup.get_text()
+    if content.find("Lyrics["):
+        start = content.find("Lyrics[")
+    else:
+        start = 0
+    end = re.search(r"[0-9][\w\s]+Embed", content)
+    if end:
+        end = len(content) if end == -1 else end.start()
+    else:
+        return
+    return content[start:end]
 
 
 if __name__ == "__main__":
