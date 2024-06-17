f281a20b3b1d2a931a480790b05cff687222bc04
ragwunobi <166466594+ragwunobi@users.noreply.github.com>
2024-06-16 22:49:38 -0400

    refactor: Refactor app.py into config.py, utils.py, and app.py
    - Refactor app.py into three files:
    - app.py has routes for flask app (/songs and /features)
    - utils.py has helper functions (get_song_lyrics, clean_lyrics)
    - config.py has global variables (env variables, headers, and dictionaries)

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,12 @@
-from flask import Flask, jsonify
-from bs4 import BeautifulSoup
+from flask import Flask
 import requests
 import json
 import re
-import os
-from dotenv import load_dotenv
+from config import headers, unicode_dict 
+from utils import search, parse_song 
 
-# API Keys
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
-client_access = os.getenv("client_access")
-bearer = os.getenv("bearer_token")
-headers = {"Authorization": f"Bearer {bearer}"}
 app = Flask(__name__)
 
-# Unicode : plain text dictionary
-unicode = {"\u00a0": " ", "\u2019": "'", "\u200b": " ", "\u0435": "e"}
-
-
-def search(keyword, url, params={}):
-    """Search Genius API for keyword
-    Parameters:
-    keyword(str): The search string
-    url(str): URL of Genius API endpoint
-    params(dict): Optional parameters to request Genius API (e.x. page (page number), per_page (results per page))
-    Returns:
-    Response object or raises an exception
-    """
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        return response
-    except requests.exceptions.Timeout:
-        raise Exception("Timeout error. Try again.")
-    except requests.exceptions.TooManyRedirects:
-        raise Exception("Keyword not found. Bad URL.")
-    except requests.exceptions.RequestException as error:
-        raise SystemExit(error)
 
 
 @app.route("/songs/<name>")
@@ -56,10 +27,9 @@ def get_songs(
     params = {"per_page": per_page, "page": start_page}
     song_data = []  # List to store artist's song lyrics and metadata
     page_count = 0
-    url = f"http://api.genius.com/search?q={name}"
     while page_count <= page_limit:
         # Search Genius API for artist name
-        response = search(name, url, params)
+        response = search(name, params)
         parse_song(response, song_data, features)
         page_count += 1
         # Increment page parameter for next request
@@ -103,150 +73,6 @@ def get_featured_artists(
         song_index += 1
 
     return artist_count
-
-
-def parse_song(response, song_data=[], features=True):
-    """Parse response object for title, path, and lyrics of each song
-    Parameters:
-    response(Requests.response): JSON response for song from Genius API GET request
-    features(bool): T/F flag to get featured artists
-    Returns:
-    (song_data(list(list)): Title, primary artists, featured artists, path, and lyrics for each song found
-    """
-    response = response.json()["response"]
-    for song in response["hits"]:
-        result = song["result"]
-        title = path = ""
-        if result["full_title"]:
-            # Replace unicode expressions from the title
-            title = remove_unicode(result["full_title"], unicode)
-        if result["path"]:
-            path = result["path"]
-        # Get list of primary_artists (sometimes a compound string e.x. Calvin Harris & Lana Del Rey)
-        primary_artists = result["primary_artists"]
-        artists = []
-        for metadata in primary_artists:
-            name = metadata["name"]
-            if name.find("&") or name.find(","):
-                multiple_artists = split_artists(name)
-                if multiple_artists:
-                    for artist in multiple_artists:
-                        artists.append(remove_unicode(artist, unicode))
-            else:
-                artists.append(name)
-        # Get list of featured artists
-        if features:
-            featured_artists = []
-            for artist in result["featured_artists"]:
-                featured_artists.append(remove_unicode(artist["name"], unicode))
-        lyrics = song_lyrics(path)
-        song_data.append([title, featured_artists, artists, path, lyrics])
-
-
-def split_artists(name, delimiter=["&", ","]):
-    """Parse out each artist name from a delimited string
-    Parameters:
-    name(string): A string of artist name(s)
-    delimiter(list): The string of possible characters the string is delimited by
-    Returns:
-    artists(list): A list of artist names parsed out from string
-    """
-    for char in delimiter:
-        if name.find(char) != -1:
-            artists = name.split(char)
-            return artists
-
-
-def song_lyrics(path):
-    """Scrape song lyrics and clean text
-    Parameters:
-    path(str): Path of song to scrape with GET request
-    Returns:
-    lyrics(str): Cleaned string of song lyrics or raises an exception
-    """
-    url = f"https://genius.com{path}"
-    try:
-        # Scrape song lyrics from Genius site
-        response = requests.get(url, headers=headers)
-        # Clean lyrics and return
-        lyrics = clean_lyrics(response)
-        return lyrics
-    except requests.exceptions.Timeout:
-        raise Exception("Timeout error. Try again.")
-    except requests.exceptions.TooManyRedirects:
-        raise Exception("Path not found. Bad URL.")
-    except requests.exceptions.RequestException as error:
-        raise SystemExit(error)
-
-
-def remove_unicode(content, unicode_dict={}):
-    """Replace unicode expressions from a string with plain text characters
-    Parameters:
-    content(str): String to remove characters from
-    unicode(dict): Optional dictionary of (target unicode string : replacement plaintext string) pairs
-    Returns:
-    content(str): Input string with unicode key values replaced with plaintext dict values
-    """
-    for key in unicode_dict:
-        content = content.replace(key, unicode_dict[key])
-    return content
-
-
-def insert_spaces(content, regex=[]):
-    """Insert space betweeen two regex groups
-    Parameters:
-    content(str): String to insert spaces into
-    regex(list):  Optional List of regex patterns, each pattern has two groups
-    Returns:
-    content(str): Input string with space inserted between each regex pattern
-    """
-    for pattern in regex:
-        content = re.sub(pattern, r"\1 \2", content)
-    return content
-
-
-def remove_end_digits(content):
-    """Remove/clean digits at the end of lyrics
-    Parameters:
-    content(str): String to remove digits from the end of
-    Returns:
-    content(str): Input string with digits at the end removed
-    """
-    end = len(content) - 1
-    while content[end].isdigit():
-        content = content[0:end]
-        end = len(content) - 1
-    return content
-
-
-def clean_lyrics(response, start_pattern=r"Lyrics[", end_pattern=r"Embed"):
-    """Parse and clean lyrics from response object
-    Parameters:
-    response(requests.response): Response object from GET Request
-    start_pattern(str): Optional string pattern to match start of lyrics
-    end_pattern(str): Optional string pattern to match end of lyrics
-    Returns:
-    content(str): String of cleaned lyrics (digits at the end removed, unicode replaced with plain text, and spaces inserted)
-    """
-    soup = BeautifulSoup(response.text, "html.parser")
-    # Get plain text from response
-    content = soup.get_text()
-    # Find index where lyrics start
-    start = content.find(start_pattern)
-    if start == -1:
-        start = 0
-    # Find index where lyrics end
-    end = content.find(end_pattern)
-    if end == -1:
-        end = len(content) - 1
-    # Call helper functions to clean lyrics
-    content = content[start:end]
-    content = remove_end_digits(content)
-    content = remove_unicode(content, unicode)
-    content = insert_spaces(
-        content, regex=[r"([a-z])([A-Z])", r"(])([A-Z])", r"(\))([A-Z])"]
-    )
-    return content
 
 
 if __name__ == "__main__":
---
+++ backend/utils.py
@@ -0,0 +1,187 @@
+import requests
+from config import headers, unicode_dict 
+from bs4 import BeautifulSoup
+import re
+
+def search(keyword, params={}):
+    """Search Genius API for a keyword (e.x. artist name)
+    Parameters:
+    keyword(str): The input search string
+    params(dict): Optional parameters dictionary (e.x. page number, results per page)
+    Returns:
+    Response object or raises an exception
+    """
+    try:
+        url = f"http://api.genius.com/search?q={keyword}"
+        response = requests.get(url, headers=headers, params=params)
+        return response
+    except requests.exceptions.Timeout:
+        raise Exception("Timeout error. Try again.")
+    except requests.exceptions.TooManyRedirects:
+        raise Exception("Keyword not found. Bad URL.")
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+
+
+def split_artists(name, delimiter=["&", ","]):
+    """Split a delimited string of artists names into an array 
+    Parameters:
+    name(string): An input string of artist name(s)
+    delimiter(list): List of characters that could delimit artists names in input string 
+    Returns:
+    artists(list): A list of individual artist names parsed from input string 
+    """
+    # Find which character is used as a delimiter 
+    for char in delimiter:
+        if name.find(char) != -1: 
+            # Split name into list of individual artist names 
+            artists = name.split(char)
+            return artists  
+
+def parse_song(response, song_data=[], features=True):
+    """Get title, path, and lyrics for each song in a response object 
+    Parameters:
+    response(Requests.response): JSON response from Genius API GET request
+    features(bool): Input boolean flag to get a song's featured_artists 
+    Returns:
+    song_data(list(list)): Title, primary artists, featured artists (opt), path, and lyrics for each song found
+    """
+    response = response.json()["response"]
+    # Iterate through each song in response 
+    for song in response["hits"]:
+        result = song["result"]
+        title = path = ""
+
+        # Get song title from response 
+        if result["full_title"]:
+            # Replace unicode expressions from the title
+            title = remove_unicode(result["full_title"], unicode_dict)
+        # Get song's path 
+        if result["path"]:
+            path = result["path"]
+        # Get list of primary_artists 
+        primary_artists = []
+        for metadata in result["primary_artists"]:
+            name = metadata["name"]
+            # Primary artist is sometimes  a string of multiple artists (e.x. Calvin Harris & Lana Del Rey)
+            if '&' in name or ',' in name:
+                multiple_artists = split_artists(name)
+                if multiple_artists:
+                    for artist in multiple_artists:
+                        #  Replace unicode expressions from artist names 
+                        primary_artists.append(remove_unicode(artist, unicode_dict))
+            else:
+                primary_artists.append(name)
+        # Get list of featured artists
+        if features:
+            featured_artists = []
+            for artist in result["featured_artists"]:
+                #  Replace unicode expressions from artist names 
+                featured_artists.append(remove_unicode(artist["name"], unicode_dict))
+        # Get song lyrics using song path 
+        lyrics = song_lyrics(path)
+        song_data.append([title, featured_artists, primary_artists, path, lyrics])
+
+
+def song_lyrics(path):
+    """Scrape song lyrics and clean text
+    Parameters:
+    path(str): Path of song to scrape with GET request
+    Returns:
+    lyrics(str): Cleaned string of song lyrics or raises an exception
+    """
+    url = f"https://genius.com{path}"
+    try:
+        # Scrape song lyrics from Genius site
+        response = requests.get(url, headers=headers)
+        # Clean lyrics and return
+        lyrics = clean_lyrics(response)
+        return lyrics
+    except requests.exceptions.Timeout:
+        raise Exception("Timeout error. Try again.")
+    except requests.exceptions.TooManyRedirects:
+        raise Exception("Path not found. Bad URL.")
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+
+
+def clean_lyrics(response, start_pattern=r"Lyrics[", end_pattern=r"Embed"):
+    """Parse and clean lyrics from Response object 
+    Remove digits at the end of lyrics, replace unicode expressions with plaintext, insert spaces
+    Parameters:
+    response(Requests.response): Response object from GET Request
+    start_pattern(str): Optional string pattern to match start of lyrics
+    end_pattern(str): Optional string pattern to match end of lyrics
+    Returns:
+    lyrics(str): String of cleaned lyrics 
+    """
+    soup = BeautifulSoup(response.text, "html.parser")
+    # Get plain text from response
+    lyrics = soup.get_text()
+    # Find index where lyrics start
+    start = lyrics.find(start_pattern)
+    if start == -1:
+        start = 0
+    # Find index where lyrics end
+    end = lyrics.find(end_pattern)
+    if end == -1:
+        end = len(lyrics) - 1
+    # Call helper functions to clean lyrics
+    lyrics = lyrics[start:end]
+    # Remove digits at the end of lyrics 
+    lyrics = remove_end_digits(lyrics)
+    # Replace unicode expressions with plaintext 
+    lyrics = remove_unicode(lyrics, unicode_dict)
+    # Use Regex matching to insert spaces 
+    lyrics = insert_spaces(
+        lyrics, regex=[r"([a-z])([A-Z])", r"(])([A-Z])", r"(\))([A-Z])"]
+    )
+    return lyrics 
+
+
+def remove_end_digits(content):
+    """Cleans digits at the end of a string 
+    Parameters:
+    content(str): Input string 
+    Returns:
+    content(str): Input string with digits at the end removed
+    """
+    # Last index of input string 
+    end = len(content) - 1
+    # Remove digits at the end of the string 
+    while content[end].isdigit():
+        content = content[0:end]
+        # Recalculate last index 
+        end = len(content) - 1
+    return content
+
+
+def insert_spaces(content, regex=[]):
+    """Insert space betweeen two regex groups
+    Parameters:
+    content(str): Input string to insert spaces into
+    regex(list):  Optional list of regex patterns (each patttern has two groups) 
+    Returns:
+    content(str): Input string with space inserted between each regex pattern
+    """
+    for pattern in regex:
+        # Match regex pattern and insert a space between groups 
+        content = re.sub(pattern, r"\1 \2", content)
+    return content
+
+
+def remove_unicode(content, unicode_dict={}):
+    """Replace unicode expressions from a string with plaintext characters
+    Parameters:
+    content(str): Input string to remove unicode expressions from 
+    unicode(dict): Optional dictionary of (target unicode string : replacement plaintext string) pairs
+    Returns:
+    content(str): Input string with dictionary's unicode keys replaced with plaintext values
+    """
+    # Replace each unicode_dict key in content with value 
+    for key in unicode_dict:
+        content = content.replace(key, unicode_dict[key])
+    return content
+
+
+
